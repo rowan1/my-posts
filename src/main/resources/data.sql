ea4feac3158c06419005d64ba4f1572d9467412d
Rowan Ibrahim <rowan.ibrahim@yomicepa.com>
2020-08-25 23:22:00 +0200

    add data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,4 @@
-INSERT INTO USER (user_name, full_name, password ) VALUES ('rowan', 'rowan ibrahim','$2y$12$M7pR1A10v2sTkOLGm/obbeohehbCHRvvW622Zmy8DWPwBAiFThC4q');
+INSERT INTO USER (user_name, full_name, password )
+VALUES ('test', 'first user','$2y$12$M7pR1A10v2sTkOLGm/obbeohehbCHRvvW622Zmy8DWPwBAiFThC4q'),
+('test_1', 'second user','$2y$12$M7pR1A10v2sTkOLGm/obbeohehbCHRvvW622Zmy8DWPwBAiFThC4q'),
+('test_2', 'third user','$2y$12$M7pR1A10v2sTkOLGm/obbeohehbCHRvvW622Zmy8DWPwBAiFThC4q');
